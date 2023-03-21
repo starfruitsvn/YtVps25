91692208ed8deb1975962e2e40c6106cd20ae3eb
starfruitsvn <125777410+starfruitsvn@users.noreply.github.com>
2023-03-21 16:17:57 +0700

    Update sqinstall.sh

--- sqinstall.sh
+++ sqinstall.sh
@@ -2,8 +2,7 @@ yum install squid
 systemctl start squid
 systemctl enable squid
 systemctl status squid
-curl -s "https://raw.githubusercontent.com/starfruitsvn/YtVps25/main/squid.conf"
-cp squid.conf /etc/squid/squid.conf
+curl -o /etc/squid/squid.conf "https://raw.githubusercontent.com/starfruitsvn/YtVps25/main/squid.conf"
 systemctl restart squid
 firewall-cmd --permanent --add-port=3128/tcp
 firewall-cmd --reload
